4378a67a03c1af617390c30743e72c4e886610a9
ozanil <93652740+ozanil@users.noreply.github.com>
2023-08-03 21:02:06 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 import random
 
 app = Flask(__name__)
+app.add_url_rule('/favicon.ico',
+                 redirect_to=url_for('static', filename='favicon.ico'))
 
 error_messages = {
     "SyntaxError": [
